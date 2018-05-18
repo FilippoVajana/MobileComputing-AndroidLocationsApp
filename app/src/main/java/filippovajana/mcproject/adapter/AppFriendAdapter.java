51f32ebd2959dc2377ab5ef9bdbf819094554e7a
FilippoVajana <pippovaja@gmail.com>
2018-05-18 18:38:59 +0200

    add 'friend' list item adapter

---
+++ app/src/main/java/filippovajana/mcproject/adapter/AppFriendAdapter.java
@@ -0,0 +1,59 @@
+package filippovajana.mcproject.adapter;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import filippovajana.mcproject.R;
+import filippovajana.mcproject.model.AppDataModel;
+import filippovajana.mcproject.model.AppFriend;
+
+public class AppFriendAdapter extends ArrayAdapter<AppFriend>
+{
+    private Context _context;
+    private List<AppFriend> _friendsList;
+
+    public AppFriendAdapter(@NonNull Context context, @NonNull List<AppFriend> list)
+    {
+        super(context, 0, list);
+
+        _context = context;
+        _friendsList = list;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        View listItem = convertView;
+        if(listItem == null)
+            listItem = LayoutInflater.from(_context).inflate(R.layout.friend_list_item,parent,false);
+
+        //get item
+        AppDataModel model = AppDataModel.getInstance();
+        AppFriend friend = model.getItem(position);
+
+        //set name
+        TextView nameText = (TextView) listItem.findViewById(R.id.textView_name);
+        nameText.setText(friend.getUsername());
+
+        //set distance
+        TextView distanceText = (TextView) listItem.findViewById(R.id.textView_distance);
+        //TODO: use geoposition service getDistance()
+
+        //set message
+        TextView messageText = (TextView) listItem.findViewById(R.id.textView_message);
+        messageText.setText(friend.getMessage());
+
+        return listItem;
+    }
+}
--- app/src/main/java/filippovajana/mcproject/model/AppDataModel.java
+++ app/src/main/java/filippovajana/mcproject/model/AppDataModel.java
@@ -1,6 +1,31 @@
 package filippovajana.mcproject.model;
 
+import java.util.ArrayList;
+
 public class AppDataModel
 {
+    //singleton
+    private static AppDataModel _instance;
+    private AppDataModel()
+    {
+        _friendsList = new ArrayList<>();
+    }
+    public static AppDataModel getInstance()
+    {
+        if (_instance == null)
+            _instance = new AppDataModel();
+        return _instance;
+    }
 
+
+    private ArrayList<AppFriend> _friendsList;
+
+    //list operations
+    private void buildModel(){}
+    private void addItem(AppFriend item){}
+    private void removeItem(AppFriend item){}
+    public AppFriend getItem(int position)
+    {
+        return _friendsList.get(position);
+    }
 }
