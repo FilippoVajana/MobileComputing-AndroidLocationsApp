f67b4a4e57dafe41d0c5c1653cc1ec3b0455f89a
Filippo Vajana <filippovajana@live.com>
2018-05-25 19:15:57 +0200

    add fragments helper class

--- app/src/main/java/filippovajana/mcproject/activity/MainActivity.java
+++ app/src/main/java/filippovajana/mcproject/activity/MainActivity.java
@@ -1,16 +1,16 @@
 package filippovajana.mcproject.activity;
 
-import android.support.annotation.IdRes;
+
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.FrameLayout;
 
 import filippovajana.mcproject.R;
 import filippovajana.mcproject.fragment.ProfileFragment;
+import filippovajana.mcproject.helper.FragmentHelper;
 
 
 public class MainActivity extends AppCompatActivity
@@ -37,33 +37,25 @@ public class MainActivity extends AppCompatActivity
 
     private void setupBottomNavigationView()
     {
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)
-                findViewById(R.id.bottomMenu);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomMenu);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(
-                item -> {
-                    switch (item.getItemId()) {
+                item ->
+                {
+                    switch (item.getItemId())
+                    {
                         case R.id.action_profile:
-
+                            FragmentHelper.loadFragment(getSupportFragmentManager(),
+                                    FragmentHelper.fragmentDictionary.get("Profile"),
+                                    findViewById(R.id.fragment_container));
                         case R.id.action_status_update:
 
                         case R.id.action_add_friend:
 
                         case R.id.action_friends_list:
-                            loadFragment(new ProfileFragment());
 
                     }
                     return true;
                 });
-    }
-
-
-
-    //TODO: safe fragment commit
-    private void loadFragment(Fragment fragment)
-    {
-        //add fragment
-        _fragmentTransaction.add(R.id.fragment_container, fragment);
-        _fragmentTransaction.commit();
     }
 }
