66d8104204a976ff4a3bf504741fcd883207e3d2
Filippo Vajana <filippovajana@live.com>
2018-06-15 00:10:40 +0200

    minor layout change

--- app/src/main/java/filippovajana/mcproject/fragment/FriendsMapFragment.java
+++ app/src/main/java/filippovajana/mcproject/fragment/FriendsMapFragment.java
@@ -1,17 +1,30 @@
 package filippovajana.mcproject.fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import filippovajana.mcproject.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class FriendsMapFragment extends Fragment
+import filippovajana.mcproject.R;
+import filippovajana.mcproject.location.LocationManager;
+
+public class FriendsMapFragment extends Fragment implements OnMapReadyCallback
 {
+    //view
+    private View _view;
+
+    //map view
+    MapView _mapView;
+
+    //Google Map
+    private GoogleMap _map;
+    private LocationManager _locationManager;
+
 
     public FriendsMapFragment()
     {
@@ -27,10 +40,53 @@ public class FriendsMapFragment extends Fragment
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends_map, container, false);
+        _view = inflater.inflate(R.layout.fragment_friends_map, container, false);
+
+        //get mapView
+        _mapView = (MapView) _view.findViewById(R.id.mapView);
+        _mapView.onCreate(savedInstanceState);
+
+        //get map
+        _mapView.getMapAsync(this);
+
+        return _view;
+    }
+
+    //Map Lifecycle
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        _mapView.onResume();
+    }
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        _mapView.onDestroy();
+    }
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        _mapView.onPause();
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        //init local map reference
+        _map = googleMap;
+
+        //init location manager
+        _locationManager = new LocationManager(this, googleMap);
+
+        //TODO: get user location
+
+        //TODO: set map bounds
+
+        //TODO: draw users markers
     }
 }
